14ca41900a35aa2fbb6cf076456a69e36d597464
BuildTools <unconfigured@null.spigotmc.org>
2019-02-24 12:46:57 +0100

    Remove capabilities

--- src/main/java/com/dombear/lineagecraft/items/ItemAmuletOfLost.java
+++ src/main/java/com/dombear/lineagecraft/items/ItemAmuletOfLost.java
@@ -1,9 +1,11 @@
 package com.dombear.lineagecraft.items;
 
+import net.minecraft.item.Item;
+
 public class ItemAmuletOfLost extends LineageCraftItemBase{
 
 	public ItemAmuletOfLost(String name) {
 		super(name);
+		this.maxStackSize = 1;
 	}
-
 }
--- src/main/java/com/dombear/lineagecraft/utils/handlers/LineageCraftEventHandler.java
+++ src/main/java/com/dombear/lineagecraft/utils/handlers/LineageCraftEventHandler.java
@@ -1,7 +1,6 @@
 package com.dombear.lineagecraft.utils.handlers;
 
 import com.dombear.lineagecraft.events.BrokenArmorEvents;
-import com.dombear.lineagecraft.events.ExtendedInventoryEvents;
 import com.dombear.lineagecraft.events.SoulShotEvents;
 
 import net.minecraftforge.common.MinecraftForge;
@@ -13,6 +12,5 @@ public class LineageCraftEventHandler {
 		MinecraftForge.EVENT_BUS.register(new SoulShotEvents());
 //		MinecraftForge.EVENT_BUS.register(new PlayerEvents());
 		MinecraftForge.EVENT_BUS.register(new BrokenArmorEvents());
-		MinecraftForge.EVENT_BUS.register(new ExtendedInventoryEvents());
 	}
 }
